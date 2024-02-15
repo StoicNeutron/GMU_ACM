a13974d3d82c2e30f4123a24e4863dafe1b23f18
AMATAKYVI <yviamatak123@gmail.com>
2024-02-14 23:20:27 -0500

    pages: /home and /about

---
+++ web/src/pages/about/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+interface AboutProps {}
+const About: React.FC<AboutProps> = () => {
+  return <div className="">About</div>;
+};
+export default About;
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,14 @@
 import { Inter } from 'next/font/google';
-
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+const Home: React.FC = ({}) => {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/home');
+  }, [router]);
   return <main></main>;
-}
+};
+
+export default Home;
