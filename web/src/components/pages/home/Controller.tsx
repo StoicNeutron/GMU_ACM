b9a5444a65ee34bbe43c32c3dd27108911361dc2
Pete Pongpeauk <kurtsiberg@gmail.com>
2024-02-20 14:47:13 -0500

    feat(home) add a row divider to the controller tile

--- web/src/components/pages/home/Controller.tsx
+++ web/src/components/pages/home/Controller.tsx
@@ -1,4 +1,4 @@
-import { Flex, Title } from "@mantine/core";
+import { Box, Divider, Flex, Title } from "@mantine/core";
 
 const styles = {
   container: {
@@ -6,12 +6,20 @@ const styles = {
     padding: 16,
     borderRadius: 2,
   },
+  divider: {
+    border: "2px solid black",
+  },
 } as Record<string, React.CSSProperties>;
 
 export default function Component() {
   return (
-    <Flex flex={1} style={styles.container}>
-      <Title order={2}>Controller</Title>
+    <Flex flex={1} direction={"column"} style={styles.container}>
+      {/* user input */}
+      <Box id="user-input" flex={1} />
+      {/* divider */}
+      <Divider style={styles.divider} />
+      {/* analysis */}
+      <Box id="analysis" flex={1} />
     </Flex>
   );
 }
