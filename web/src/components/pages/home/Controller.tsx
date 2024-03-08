6ef885127b2cace0f735ee83322c176d28a979b9
StoicNeutron <stoicneutron@Samnangs-MacBook-Pro.local>
2024-03-08 14:54:18 -0500

    change style

--- web/src/components/pages/home/Controller.tsx
+++ web/src/components/pages/home/Controller.tsx
@@ -1,4 +1,5 @@
-import { Box, Divider, Flex } from '@mantine/core';
+import { Box, Divider, Flex, Select, Button, MantineProvider } from '@mantine/core';
+import React, { useState } from 'react';
 
 const styles = {
   container: {
@@ -9,42 +10,57 @@ const styles = {
   divider: {
     border: '2px solid black',
   },
+  h4: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: '25px'
+  },
+  button: {
+    position: 'relative',
+    zIndex: 1, // Ensure button appears above other elements
+  },
 } as Record<string, React.CSSProperties>;
 
+const algorithmsOptions = [
+  { value: '1', label: 'DFS' },
+  { value: '2', label: 'BFS' },
+];
+
+const speedOptions = [
+  { value: '1', label: '1X' },
+  { value: '2', label: '2X' },
+];
+
 export default function Component() {
+  const [selectedValue, setSelectedValue] = useState<string | null>(null);
+
+  const handleChange = (value: string | null, option: { value: string; label: string }) => {
+    setSelectedValue(value);
+  };
   return (
     <Flex flex={1} direction={'column'} style={styles.container}>
-      {/* user input > algorithm and speed */}
-      <Box id="user-input" flex={1}>
-        <div className="flex gap-5">
-          <button className="text-sm bg-blue-400 hover:bg-blue-500 transition duration-200 font-semibold px-10 py-1 rounded text-white ">
-            Generate
-          </button>
-          <button className="text-sm bg-blue-400 hover:bg-blue-500 transition duration-200 font-semibold px-10 py-1 rounded text-white ">
-            Run
-          </button>
-        </div>
-        <div className="mt-3">
-          <div className="flex gap-10 items-center">
-            <label className="text-sm font-semibold w-[50px]">Algorithm:</label>
-            <select className="origin-bottom px-3 text-sm rounded border border-gray-300 w-[250px] appearance-none text-center">
-              <option>Select Algorithm</option>
-              <option>Algo 1</option>
-              <option>Algo 2</option>
-            </select>
-          </div>
-          <div className="flex gap-10 items-center mt-5">
-            <label className="text-sm font-semibold w-[50px]">Speed</label>
-            <select className="origin-bottom px-3 text-sm rounded border border-gray-300 w-[250px] appearance-none text-center">
-              <option>1x</option>
-              <option>2x</option>
-            </select>
-          </div>
-        </div>
-      </Box>
-      {/* divider */}
+      <h4 style={styles.h4}>Controller</h4>
+
+      <Select
+        data={algorithmsOptions}
+        value={selectedValue}
+        onChange={handleChange}
+        placeholder="Selected Algorithm"
+        style={{ marginBottom: '20px' }}
+      />
+
+      <Select
+        data={speedOptions}
+        value={selectedValue}
+        onChange={handleChange}
+        placeholder="Selected Speed"
+        style={{ marginBottom: '20px' }}
+      />
+
+      <Button variant="filled" color="green" size="md" radius="md" style={{ marginBottom: '20px' }}>RESET</Button>
+      <Button variant="filled" color="green" size="md" radius="md" style={{ marginBottom: '20px' }}>RESET</Button>
       <Divider style={styles.divider} />
-      {/* analysis */}
+
       <Box id="analysis" flex={1} />
     </Flex>
   );
