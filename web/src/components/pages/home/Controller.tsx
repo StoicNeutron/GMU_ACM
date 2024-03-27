ec02f8b2b7b13bbb6c3b65d93ecb9874b02caa32
StoicNeutron <stoicneutron@Samnangs-MacBook-Pro.local>
2024-03-27 11:01:35 -0400

    implement controller botton part

--- web/src/components/pages/home/Controller.tsx
+++ web/src/components/pages/home/Controller.tsx
@@ -17,13 +17,13 @@ const styles = {
   },
   button: {
     position: 'relative',
-    zIndex: 1, // Ensure button appears above other elements
+    zIndex: 1,
   },
 } as Record<string, React.CSSProperties>;
 
 const algorithmsOptions = [
-  { value: '1', label: 'DFS' },
-  { value: '2', label: 'BFS' },
+  { value: '1', label: 'DFS', fullName: 'Depth First Search', des: 'Depth-First Search (DFS) is a graph traversal algorithm that explores as far as possible along each branch before backtracking. It prioritizes depth, commonly used for pathfinding and cycle detection.' },
+  { value: '2', label: 'BFS', fullName: 'Breadth First Search', des: 'Breadth-First Search (BFS) is a graph traversal algorithm that explores neighboring nodes before moving deeper. It uses a queue to ensure nodes are visited in the order they were discovered. BFS is efficient for finding the shortest path in unweighted graphs and exploring tree structures.' },
 ];
 
 const speedOptions = [
@@ -35,15 +35,33 @@ export default function Component() {
   const [selectedAlgorithm, setSelectedAlgorithm] = useState<string | null>(
     null
   );
+  const [selectedAlgorithmName, setSelectedAlgorithmName] = useState<string | null>(
+    null
+  );
+  const [selectedAlgorithmDes, setSelectedAlgorithmDes] = useState<string | null>(
+    null
+  );
   const [selectedSpeed, setSelectedSpeed] = useState<string | null>(null);
 
   const handleAlgorithmChange = (value: string | null) => {
     setSelectedAlgorithm(value);
+    if (value !== null) {
+      const selectedAlgorithmObject = algorithmsOptions.find(option => option.value === value);
+      if (selectedAlgorithmObject) {
+        setSelectedAlgorithmName(selectedAlgorithmObject.fullName);
+        setSelectedAlgorithmDes(selectedAlgorithmObject.des);
+      }
+    } else {
+      setSelectedAlgorithmName(null);
+    }
   };
 
   const handleSpeedChange = (value: string | null) => {
     setSelectedSpeed(value);
   };
+
+  const isButtonsDisabled = selectedAlgorithm === null || selectedSpeed === null;
+
   return (
     <Flex flex={1} direction={'column'} style={styles.container}>
       <h4 style={styles.h4}>Controller</h4>
@@ -70,6 +88,7 @@ export default function Component() {
         size="md"
         radius="md"
         style={{ marginBottom: '20px' }}
+        disabled={isButtonsDisabled}
       >
         RUN
       </Button>
@@ -79,10 +98,14 @@ export default function Component() {
         size="md"
         radius="md"
         style={{ marginBottom: '20px' }}
+        disabled={isButtonsDisabled}
       >
         RESET
       </Button>
       <Divider style={styles.divider} />
+
+      <h3>Algorithm: {selectedAlgorithmName}</h3>
+      <p>{selectedAlgorithmDes}</p>
 
       <Box id="analysis" flex={1} />
     </Flex>
