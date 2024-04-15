6596ab153d8d1b44077a40edf6cfdd69eb03dc79
Samnang Thorn <83148244+StoicNeutron@users.noreply.github.com>
2024-04-15 10:47:19 -0400

    Update index.tsx

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -8,7 +8,7 @@ import Visualizer from "@/components/pages/home/Visualizer";
 function Page() {
   const [signal, setSignal] = useState<boolean>(false); // Lift state up to Page component
 
-  const handleRun = (algorithm, speed) => {
+  const handleRun = (algorithm: string, speed: number) => {
     // Handle the selected algorithm and speed data here
     console.log("Selected Algorithm:", algorithm);
     console.log("Selected Speed:", speed);
